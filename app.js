12a0eed7881034b5a64f3602e5857b0d622efa0b
Vitao127 <coprixgamer@gmail.com>
2025-12-29 13:57:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbx3Kbn2qD8DL7xKlAM_ZcmPXIyLABT49saZHVRNTazla6xucJr1YWaMC41c1rQwOTNbZA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyHVJHAL960ccAFh_kjbiLHGaxTogrGqaMCSc27_5yPwlU298R5DzaFqgTTJ0HyNlR2KA/exec";
 
 let currentDriver = null;
 const input = document.getElementById("scannerInput");
@@ -75,6 +75,7 @@ function showScreen(id) {
 }
 
 input.focus();
+
 
 
 
